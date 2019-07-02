fb63f4c19494db5facd81fcd658d413d81d43527
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-07-02 11:40:24 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Recipes from './Recipes';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Recipes
+      </header>
+      <Recipes/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Recipes.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+   class Recipes extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+           recipe: "Select a recipe above"};
+    
+        this.handleClick = this.handleClick.bind(this);
+      }
+    
+      handleClick(recipeUpdated) {
+        this.setState(state => ({
+          isToggleOn: !state.isToggleOn,
+          recipe: recipeUpdated,
+        }));
+      }
+    
+      render() {
+        return (
+           <div>
+          <button onClick={() => this.handleClick("Banana ice cream recipe")}>
+            Banana Ice Cream
+          </button>
+          <button onClick={() => this.handleClick("Stew recipe")}>
+            Stew
+          </button>
+          <button onClick={() => this.handleClick("Goat's Head Soup recipe")}>
+            Goat's Head Soup
+          </button>
+          <p>{this.state.recipe}</p>
+          </div>
+        );
+      }
+    }
+
+    export default Recipes;
