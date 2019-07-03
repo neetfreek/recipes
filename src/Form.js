c2e5bf83b2d96c4d1f8559176e1a0ff78bba891e
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-07-03 11:25:23 +0300

    Create Form.js
    To be used to user-submitted search for recipes

---
+++ src/Form.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class Form extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            textField: "",
+            textSubmitted: "",
+          };
+
+        this.onChange= this.onChange.bind(this);
+        this.onSubmit= this.onSubmit.bind(this);
+    }
+
+    onChange = event => {
+        this.setState({textField: event.target.value
+        });
+    }
+
+    onSubmit = event => {
+        this.setState({
+            textSubmitted: this.state.textField,
+        });
+        event.preventDefault();
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.onSubmit}>
+                <label>
+                    Recipe:
+                    <input type= "text" value= {this.state.textField} onChange={this.onChange}/>
+                </label>
+                <input type="submit" value="Search"/>
+            </form>
+        );
+    }
+}
+
+export default Form;
