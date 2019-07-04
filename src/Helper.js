459311f48af79171561325074493bef8f05e8f51
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-07-04 11:16:39 +0300

    Simplify listComponentFromArray using array.map; add keys to <li> children

--- src/Helper.js
+++ src/Helper.js
@@ -1,25 +1,16 @@
 import React from 'react';
 
 function listComponentFromArray(array){
-    const list = array;
-    var listArray = [];
-
-    for (var counter = 0; counter < list.length; counter++){
-      var itemName = list[counter];
-      var listItem = React.createElement('li', {}, itemName);
-      listArray.push(listItem);
-    }
-    var listElement = React.createElement('ul', {}, [listArray]);
-    var listContainer = React.createElement('div', {className: "listIngredients"}, [listElement]);
+    const listItems = array.map((item, index) => <li key={index}>{item}</li>);
 
     return (
-      listContainer
+        <div className="listIngredients"><ul>{listItems}</ul></div>
     );
   }
 
   export function findItemByName(array, nameItem){
     for (var counter = 0; counter < array.length; counter++){
-      if (array[counter].name == nameItem){
+      if (array[counter].name === nameItem){
         return array[counter];
       }
     }
